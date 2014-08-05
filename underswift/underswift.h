c6cc2dac75d4801eed3baf6d597e35a8bf49f712
John Doty <jrhdoty@gmail.com>
2014-08-05 10:53:15 -0700

    Initial Commit

---
+++ underswift/underswift.h
@@ -0,0 +1,13 @@
+//
+//  underswift.h
+//  underswift
+//
+//  Created by John Doty on 8/5/14.
+//  Copyright (c) 2014 John Doty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface underswift : NSObject
+
+@end
---
+++ underswift/underswift.m
@@ -0,0 +1,13 @@
+//
+//  underswift.m
+//  underswift
+//
+//  Created by John Doty on 8/5/14.
+//  Copyright (c) 2014 John Doty. All rights reserved.
+//
+
+#import "underswift.h"
+
+@implementation underswift
+
+@end
